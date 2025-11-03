2d6250f9550766b00604d7c2e7528525b160acd1
Mahesh M <maheshmuralimvk@gmail.com>
2025-11-03 18:02:18 +0300

    Meta data Changes And Contact Card Updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Shenzor- Industries",
+  title: "Shenzor",
   description: "Shenzor Integrated CCTV Inspection Systems",
 };
 
